b2fbd07414fd9d68eeabdccc3b473091afc74a95
Pu Anlai <inspectormustache@users.noreply.github.com>
2025-04-13 17:12:24 +0200

    Add variable injection capability

--- update_quadlets.sh
+++ update_quadlets.sh
@@ -8,14 +8,24 @@
 
 shopt -s nullglob
 
+TEMP_PREFIX=update_quadlets
 QUADLET_CONTAINER_DIR="$HOME/.config/containers/systemd"
 PROJECT_DIR="$(dirname "$0")"
+VAR_FILE="$(dirname "$0")/quadlet_vars"
+declare -A CUSTOM_VARS
 
 mkdir -p "$QUADLET_CONTAINER_DIR"
 
-rootflag=
+declare rootflag
 test "$EUID" -eq 0 || rootflag="--user"
 
+read_custom_vars() {
+    while IFS="" read -r line || [ -n "$line" ]; do
+        key="$(cut -d= -f 1 <<< "$line")"
+        value="$(cut -d= -f 2- <<< "$line")"
+        CUSTOM_VARS["$key"]="$value"
+    done < "$VAR_FILE"
+}
 
 update_container() {
     local quadlet_path outdated
@@ -28,27 +38,47 @@ update_container() {
 }
 
 restart_service_maybe() {
-    systemctl $rootflag is-active --quiet "$1.service" || return
+    systemctl "$rootflag" is-active --quiet "$1.service" || return
     echo "Restarting $1.service..."
-    systemctl $rootflag restart "$1.service"
+    systemctl "$rootflag" restart "$1.service"
 }
 
+inject_variables() {
+    # find marked variables in the container file
+    local template_vars
+    mapfile -t template_vars < <(grep -o '{-{[^}]\+}-}' "$1") 
+    # if there are none, return
+    test "${#template_vars[@]}" -eq 0 && return
+
+    # otherwise insert values from CUSTOM_VARS
+    for var in "${template_vars[@]}"; do
+        local stripped_var repl
+        stripped_var="${var:3:-3}"
+        repl=${CUSTOM_VARS["$stripped_var"]//\//\\\/}
+        sed -i "s/$var/$repl/g" "$1"
+    done
+}
 
 updated_containers=()
 for f in "$PROJECT_DIR/"**/*.container "$PROJECT_DIR/"**/*.network "$PROJECT_DIR/"**/*.volume; do
-    update_container "$f" && continue
+    # Create a copy of f so we can make edits
+    f_copy="$(mktemp -t "$TEMP_PREFIX.XXXXXXX")"
+    cp "$f" "$f_copy"
+    inject_variables "$f_copy"
+    update_container "$f_copy" && continue
     filebase="$(basename "$f")"
     updated_containers+=("${filebase%.*}")
+    rm "$f_copy"
 done
 
 if [[ ${#updated_containers[@]} -eq 0 ]]; then
     echo "No updated containers found."
     exit
 else
-    systemctl $rootflag daemon-reload
+    systemctl "$rootflag" daemon-reload
     for unit in "${updated_containers[@]}"; do
-        systemctl $rootflag is-active --quiet "$unit.service" || continue
+        systemctl "$rootflag" is-active --quiet "$unit.service" || continue
         echo "Restarting $unit.service..."
-        systemctl $rootflag restart "$unit.service" 
+        systemctl "$rootflag" restart "$unit.service" 
     done
 fi
