65564d94490049cf852248233f3fa9ea02ea1131
Pu Anlai <inspectormustache@users.noreply.github.com>
2025-04-14 19:45:11 +0200

    Add suport for *.script and *.template files

--- update_quadlets.sh
+++ update_quadlets.sh
@@ -1,34 +1,99 @@
 #!/bin/bash
 
-# This script will replace *.container files in ~/.config/containers/systemd
-# with the ones in this repository if they are not identical. If the container
-# file is replaced and an associated service is already running that service's
-# unit file will be updated and the service will be restarted. The script will
-# never start inactive services on its own.
+# This script will look through directories in its working directory and,
+# depending on the filename extension, process them in different ways. Before
+# any processing is done, placeholder fields marked with "{-{placeholder}-}" in
+# all files will be replaced with variables defined in the file `quadlet_vars'
+# in the script's working directory.
+# Variables in `quadlet_var' should be defined according to the specification
+# "variable=variable_value".
+#
+# Files will be handled in the following ways.
+#
+# *.script:
+# These are arbitrary scripts that will be executed.
+#
+# *.container/*.volume/*.network/*.service:
+# These files will be copied to $HOME/.config/containers/systemd If the
+# corresponding units are currently running `systemctl [--user] daemon-reload'
+# will be run and every running unit will be restarted
+#
+# *.template:
+# Files with this extension will take a destination path from their first line.
+# The rest of the file will be copied to that destination but ONLY if that file
+# does not exist yet. Missing directories are created in the process. File
+# permissions will be copied from the template file.
+# BE AWARE that environment variables in the destination path will *not* be
+# expanded. # line. However, you can use {-{placeholder}-} variables as part of
+# the path.
+#
+# Files are processed in the order above.
 
 shopt -s nullglob
-shopt -s globstar
 
-TEMP_DIR="$(mktemp -d)"
-QUADLET_CONTAINER_DIR="$HOME/.config/containers/systemd"
-PROJECT_DIR="$(dirname "$0")"
-VAR_FILE="$(dirname "$0")/quadlet_vars"
+TEMP_DIR=$(mktemp -d)
+PROJECT_DIR=$(dirname "$0")
+VAR_FILE=$(dirname "$0")/quadlet_vars
 declare -A CUSTOM_VARS
 
+if [[ "$EUID" -eq 0  ]]; then
+    QUADLET_CONTAINER_DIR="/etc/containers/systemd"
+else
+    QUADLET_CONTAINER_DIR="$HOME/.config/containers/systemd"
+    ROOTFLAG="--user"
+fi
+
+
 mkdir -p "$QUADLET_CONTAINER_DIR"
-
-declare rootflag
-test "$EUID" -eq 0 || rootflag="--user"
-
 
 # Beginning of global functions
 read_custom_vars() {
     while IFS="" read -r line || [ -n "$line" ]; do
-        key="$(cut -s -d= -f 1 <<< "$line")"
-        value="$(cut -s -d= -f 2- <<< "$line")"
+        key=$(cut -s -d= -f 1 <<< "$line")
+        value=$(cut -s -d= -f 2- <<< "$line")
         test -z "$key" && continue
         CUSTOM_VARS["$key"]="$value"
     done < "$VAR_FILE"
+}
+
+restart_service_maybe() {
+    systemctl "$ROOTFLAG" is-active --quiet "$1.service" || return
+    echo "Restarting $1.service..." >&2
+    systemctl "$ROOTFLAG" restart "$1.service"
+}
+
+replace_in_file() {
+    local repl
+    if [[ -v CUSTOM_VARS["$1"] ]]; then
+        repl=${CUSTOM_VARS["$1"]//\//\\\/}
+        sed -i "s/$var/$repl/g" "$2"
+    else
+        echo "Warning: $1 not defined in quadlet_vars." >&2
+    fi
+}
+
+inject_variables() {
+    # find marked variables in the container file
+    local template_vars
+    mapfile -t template_vars < <(grep -o '{-{[^}]\+}-}' "$1") 
+    # if there are none, return
+    test "${#template_vars[@]}" -eq 0 && return
+
+    # otherwise insert values from CUSTOM_VARS
+    for var in "${template_vars[@]}"; do
+        local stripped_var
+        stripped_var="${var:3:-3}"
+        replace_in_file "$stripped_var" "$1"
+    done
+}
+
+make_injected_copy() {
+    local filebase copy
+    filebase=$(basename "$1")
+    copy="$TEMP_DIR/$filebase"
+    cp "$1" "$copy"
+    inject_variables "$copy"
+    echo -n "$copy"
 }
 
 update_container() {
@@ -41,26 +106,19 @@ update_container() {
     fi
 }
 
-restart_service_maybe() {
-    systemctl "$rootflag" is-active --quiet "$1.service" || return
-    echo "Restarting $1.service..."
-    systemctl "$rootflag" restart "$1.service"
-}
+update_template() {
+    local source dest
+    source="$1"
+    dest="$2"
 
-inject_variables() {
-    # find marked variables in the container file
-    local template_vars
-    mapfile -t template_vars < <(grep -o '{-{[^}]\+}-}' "$1") 
-    # if there are none, return
-    test "${#template_vars[@]}" -eq 0 && return
+    echo "---source: $source" >&2
+    echo "---dest: $dest" >&2
 
-    # otherwise insert values from CUSTOM_VARS
-    for var in "${template_vars[@]}"; do
-        local stripped_var repl
-        stripped_var="${var:3:-3}"
-        repl=${CUSTOM_VARS["$stripped_var"]//\//\\\/}
-        sed -i "s/$var/$repl/g" "$1"
-    done
+    test -e "$dest" && return
+    mkdir -p "$(dirname "$dest")"
+    tail -n+1 "$source" > "$dest"
+    chown --reference="$source" "$dest"
+    echo "$dest created." >&2
 }
 # End of global functions
 
@@ -68,28 +126,39 @@ inject_variables() {
 # Initialize CUSTOM_VARS
 read_custom_vars
 
+# process scripts
+for f in "$PROJECT_DIR/"**/*.script; do
+    f_copy=$(make_injected_copy "$f")
+    /bin/bash "$f_copy"
+    rm "$f_copy"
+done
+
+# process containers
 updated_containers=()
 for f in "$PROJECT_DIR/"**/*.container "$PROJECT_DIR/"**/*.network "$PROJECT_DIR/"**/*.volume; do
-    # Create a copy of f so we can make edits
-    filebase="$(basename "$f")"
-    f_copy="$TEMP_DIR/$filebase"
-    cp "$f" "$f_copy"
-
-    inject_variables "$f_copy"
+    f_copy=$(make_injected_copy "$f")
     update_container "$f_copy" || updated_containers+=("${filebase%.*}")
     rm "$f_copy"
 done
 
 if [[ ${#updated_containers[@]} -eq 0 ]]; then
-    echo "No updated containers found."
-    exit
+    echo "No updates to container units." >&2
 else
-    systemctl "$rootflag" daemon-reload
+    systemctl "$ROOTFLAG" daemon-reload
     for unit in "${updated_containers[@]}"; do
-        systemctl "$rootflag" is-active --quiet "$unit.service" || continue
-        echo "Restarting $unit.service..."
-        systemctl "$rootflag" restart "$unit.service" 
+        systemctl "$ROOTFLAG" is-active --quiet "$unit.service" || continue
+        echo "Restarting $unit.service..." >&2
+        systemctl "$ROOTFLAG" restart "$unit.service" 
     done
 fi
+
+# process templates
+for f in "$PROJECT_DIR/"**/*.template; do
+    f_copy=$(make_injected_copy "$f")
+    dest=$(head -n1 "$f_copy")
+    test -e "$dest" && continue
+    update_template "$f_copy" "$dest"
+    rm "$f_copy"
+done
 
 rmdir "$TEMP_DIR"
